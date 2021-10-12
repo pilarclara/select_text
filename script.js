db77ad0f1e7721d2647c94c617e1424c63e83098
DESKTOP-0ILKU60\pilar <pilarclara@gmail.com>
2021-10-12 10:22:28 +0200

    refactorizando

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 document.querySelector("p").addEventListener("mousedown", function(){
     if (document.querySelector("p").querySelectorAll("span").length) {
-        while(document.querySelector("p").querySelector("span").firstChild)
-            document.querySelector("p").insertBefore(document.querySelector("p").querySelector("span").firstChild, document.querySelector("p").querySelector("span"));
+        document.querySelector("p").insertBefore(document.querySelector("p").querySelector("span").firstChild, document.querySelector("p").querySelector("span"));
         document.querySelector("p").removeChild(document.querySelector("p").querySelector("span"));
+        let cadena = "";
+        document.querySelector("p").childNodes.forEach(function(e){cadena+= e.textContent})
+        document.querySelector("p").innerText = cadena;
     }
 });
 
 document.querySelector("p").addEventListener("mouseup", function(){
-    if (window.getSelection().getRangeAt(0).toString().length){
-       let element = document.createElement("span");
-       window.getSelection().getRangeAt(0).surroundContents(element);
-    }
+    if (window.getSelection().getRangeAt(0).toString().length)
+       window.getSelection().getRangeAt(0).surroundContents(document.createElement("span"));
 });
